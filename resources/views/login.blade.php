9d64881ad4c031f6ff416e30ef9cd511ff775eef
Teuku Nabil <nabilmuhammad246@gmail.com>
2022-04-15 03:05:20 +0700

    Sebagian Halaman Login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>ODACLE</title>
+</head>
+
+<body>
+    <div class="container">
+        <img src="assets/images/ODACLE Logo.png" class="img-logo">
+        <div class="persegipanjang"></div>
+        <div class="btn-wrapper">
+          <a href="#" class="btn masuk">Masuk</a>
+          <p></p>
+          <a href="#" class="btn daftar">Daftar</a>
+        </div>
+    </div>
+</body>
+
+</html>
