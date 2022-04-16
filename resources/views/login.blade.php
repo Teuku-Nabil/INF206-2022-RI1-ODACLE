558d69f110a33397b5a99354976811d02ef5ea2d
Teuku Nabil <nabilmuhammad246@gmail.com>
2022-04-17 00:36:02 +0700

    Halaman login, css dan route nya

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,20 +5,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
-    <title>ODACLE</title>
+    <link rel="stylesheet" href="style/login.css">
+    <title>Login</title>
 </head>
 
 <body>
-    <div class="container">
-        <img src="assets/images/ODACLE Logo.png" class="img-logo">
-        <div class="persegipanjang"></div>
-        <div class="btn-wrapper">
-          <a href="#" class="btn masuk">Masuk</a>
-          <p></p>
-          <a href="#" class="btn daftar">Daftar</a>
+    <div class="login-form">
+        <div class=login-half>
+            <h1>MASUK</h1>
+            <form>
+                <div class="form-group">
+                    <h3>Username</h3>
+                    <input type="text" name="Username" class="form-control" placeholder="Masukkan Username">
+                </div>
+                <div class="form-group">
+                    <h3>Password</h3>
+                    <input type="password" name="password" class="form-control" placeholder="Masukkan Password">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block">MASUK</button>
+                </div>
+            </form>
+        </div>
+        <div class="reg">
+            <h3>Belum punya akun? <a href="daftar">Daftar</a></h3>
+        </div>
+        <div class="garis-tengah">      
+        </div>
+        <div class="desc">
+            <h1>Welcome Back</h1>
+            <h3>To ODACLE Website</h3>
+            <img src="assets/images/ODACLE Logo.png" class="img-logo">
+            <h3 class="slog">Take care of your teeth with us at ODACLE</h3>
         </div>
     </div>
+
 </body>
 
 </html>
