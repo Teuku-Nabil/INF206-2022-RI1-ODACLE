f699b9d67a0bf7241c16d48e3e7a48689c9c61de
maulidya amanda <lidya_ap@mhs.unsyiah.ac.id>
2022-04-25 00:00:54 +0700

    perbaikan5

--- resources/views/suksesdaftar.blade.php
+++ resources/views/suksesdaftar.blade.php
@@ -1,34 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <link rel="shortcut icon" href="assets/icon/blue.png" type="image/x-icon">
-    <link rel="stylesheet" href="style/daftar.css">
-    <link rel="stylesheet" href="style/suksesdaftar.css">
-
-    <title>sukses</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sukses/title>
 </head>
-
 <body>
-            <div class="container justify-content-center">
-    
-                <h1>SELAMAT AKUN ANDA TELAH TERDAFTAR</h1>
-                    <a href="login" class="btn masuk">Masuk</a>
-             
-            </div>
-
-    <!-- Bootstrap js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
-<body>
-
+    <div class="container justify-content-center">
+        <h1>SELAMAT AKUN ANDA TELAH TERDAFTAR</h1>
+            <a href="login" class="btn masuk">Masuk</a>
+    </div>
+</body>
 </html>
