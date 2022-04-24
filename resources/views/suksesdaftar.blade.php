718fbb676392d2fc6e0de5c0f0e5f2032867d581
maulidya amanda <lidya_ap@mhs.unsyiah.ac.id>
2022-04-25 00:14:44 +0700

    perbaikan6

--- resources/views/suksesdaftar.blade.php
+++ resources/views/suksesdaftar.blade.php
@@ -7,9 +7,6 @@
     <title>sukses/title>
 </head>
 <body>
-    <div class="container justify-content-center">
-        <h1>SELAMAT AKUN ANDA TELAH TERDAFTAR</h1>
-            <a href="login" class="btn masuk">Masuk</a>
-    </div>
+    
 </body>
 </html>
