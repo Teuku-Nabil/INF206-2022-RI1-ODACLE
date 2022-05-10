aaeb9126d6206c35ee5c254514cf070787b53d8d
Teuku Nabil <nabilmuhammad246@gmail.com>
2022-05-10 12:16:39 +0700

    fix

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -31,7 +31,7 @@ class LoginController extends Controller
             $request->session()->regenerate();
             return redirect()->intended('/beranda-dokter');
         }
-        return back()->with('error', 'Email atau Password salah');
+        return back()->with('error', 'Email atau Password Salah!');
     }
 
     public function logoutpasien()
